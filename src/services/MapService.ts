17b65eefaad7d89a0f0f6fa93ac1d5b724396c72
Jose Ramon Vieites Romay <jose.vieites@altia.es>
2023-08-15 03:04:06 +0200

    tootgle map

--- src/services/MapService.ts
+++ src/services/MapService.ts
@@ -1,7 +1,7 @@
 import Map from '@arcgis/core/Map';
 import SceneView from '@arcgis/core/views/SceneView';
-import * as windData from './wind.json';
-import type { GeoJsonFeature } from '../models/GeoJsonFeature';
+import BasemapToggle from '@arcgis/core/widgets/BasemapToggle';
+import BasemapGallery from '@arcgis/core/widgets/BasemapGallery.js';
 
 export class MapService {
   private map: Map;
@@ -9,12 +9,30 @@ export class MapService {
 
   constructor(domNode: HTMLDivElement) {
     this.map = new Map({
-      basemap: 'streets-vector',
+      basemap: 'streets',
     });
     this.view = new SceneView({
       container: domNode,
       map: new Map({ basemap: 'hybrid', ground: 'world-elevation' }),
       zoom: 8,
     });
+    this.addMapToogle();
+    //this.addBaseMapGallery();
+  }
+
+  addMapToogle() {
+    const basemapToggle = new BasemapToggle({
+      view: this.view,
+      nextBasemap: 'topo-vector',
+      visible: true,
+    });
+    this.view.ui.add(basemapToggle, 'top-right');
+  }
+
+  addBaseMapGallery() {
+    const baseMapGallery = new BasemapGallery({
+      view: this.view,
+    });
+    this.view.ui.add(baseMapGallery, 'top-right');
   }
 }
